4f7fbf63bac35795088dd59b1872da7108983e0e
Rails Udemy Course <piotr.hryniewicz84@gmail.com>
2017-09-26 03:50:41 +0200

    style errors and messages partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   def update
 
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
     # @article.save
     # redirect_to articles_path(@article)
     if @article.save
-      flash[:notice] = 'Article was succesfully created'
+      flash[:success] = 'Article was succesfully created'
       redirect_to article_path(@article)
     else
       render 'new'
@@ -39,7 +39,7 @@ class ArticlesController < ApplicationController
   def destroy
 
     @article.destroy
-    flash[:notice] = "Article was successfully deleted."
+    flash[:danger] = "Article was successfully deleted."
     redirect_to articles_path
   end
 
