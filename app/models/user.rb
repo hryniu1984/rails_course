23582ed8e5a28349a80244f02ec0bba5d88bbb4a
Rails Udemy Course <piotr.hryniewicz84@gmail.com>
2017-09-27 03:31:22 +0200

    Add secure password and atribute to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
 	has_many :articles
 	before_save { self.email = email.downcase }
@@ -7,4 +8,5 @@ class User < ActiveRecord::Base
      validates :email, presence: true, length: { maximum: 105 },
      uniqueness: { case_sensitive: false },
      format: { with: VALID_EMAIL_REGEX }
+     has_secure_password
 end
