2557c62de177a70ae1bf0df81659c1ee26ed9999
Rails Udemy Course <piotr.hryniewicz84@gmail.com>
2017-09-29 02:38:26 +0200

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 2)
@@ -11,7 +13,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
@@ -46,13 +47,19 @@ class ArticlesController < ApplicationController
   end
 
   def show
-
   end
 
   private
 
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def require_same_user
+    if curent_user != @article.user
+      flash[:danger] = "You can only edit or delete your own article"
+      redirect_to root_path
+    end
   end
 
   def article_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+	before_action :set_user, only:[:edit, :update, :show]
+	before_action :require_same_user, only: [:edit, :update]
+
 	def index
 		@users = User.paginate(page: params[:page], per_page: 5)
 	end
@@ -11,29 +14,37 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
-			redirect_to articles_path
+			redirect_to user_path(@user)
 		else
 			render 'new'
 		end
 	end
 
 	def edit
-		@user = User.find(params[:id])
 	end
 
-	def update
-		@user = User.find(params[:id])
+	def update	
 		if @user.update(user_params)
 			flash[:success] = "Your account was updated successfully"
 			redirect_to articles_path
 		else
 			render 'edit'
-		end	
+		end
 	end
 
 	def show
-		@user = User.find(params[:id])
 		@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+	end
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def require_same_user
+		if  current_user != @user
+			flash[:danger] = "You can only edit your own account"
+			redirect_to root_path
+		end
 	end
 
 	private
